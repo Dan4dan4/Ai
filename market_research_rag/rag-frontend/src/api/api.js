98b6aca3695ab65d56702b8e76e4cb6a0c5537e8
Danish <danm44@hotmail.com>
2026-02-06 15:08:36 -0500

    create a base api function file in api.js

---
+++ market_research_rag/rag-frontend/src/api/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+// this file sets base backend url 
+// creates reusable functions
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json"
+  }
+});
+
+// this function asks question
+export const askRag = (query) => {
+  return api.post("/rag/ask/", { query });
+};
+
+// this function uploads documents
+export const uploadDocument = (formData) => {
+  return api.post("/rag/upload/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  });
+};
+
+export default api;
