bcf78022494267d2855c145c456844f38b8efadd
Danish <danm44@hotmail.com>
2026-02-06 15:18:03 -0500

    downloaded axios, created ragchat.jsx but no css

---
+++ market_research_rag/rag-frontend/src/components/rag/RagChat.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { askRag } from "../../api/api";
+
+export default function RagChat() {
+    const [query, setQuery] = useState("");
+    const [response, setResponse] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async () => {
+        try {
+            setLoading(true);
+            const res = await askRag(query);
+
+            setResponse(res.data.answer);
+
+        }
+        catch (err){
+            setResponse("Error connecting to RAG service");
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+
+    return (
+    <div>
+      <h2>RAG Assistant</h2>
+
+      <textarea
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Ask a question..."
+      />
+
+      <button onClick={handleSubmit}>
+        Ask
+      </button>
+
+      {loading && <p>Thinking...</p>}
+
+      {response && (
+        <div>
+          <h4>Response</h4>
+          <p>{response}</p>
+        </div>
+      )}
+    </div>
+  );
+}
